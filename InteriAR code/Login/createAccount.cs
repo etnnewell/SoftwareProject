e64fbfdee75c01a08c360b50bc650579236b4d8d
Ishtiyaq Ali <ishtiyaq93@gmail.com>
2018-04-29 10:42:54 +0100

    Update createAccount.cs

--- InteriAR code/Login/createAccount.cs
+++ InteriAR code/Login/createAccount.cs
@@ -47,7 +47,9 @@ public class createAccount : MonoBehaviour {
     System.Text.RegularExpressions.Regex mailValidator = new System.Text.RegularExpressions.Regex(@"^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$");
 
    
-    public void Update()
+    public void Update()    //this method essentially just validates all input fields whenever user edits their profile. for example,
+	                    //the password and confirm password fields much match. if not, then an error message will display and the
+                            //button that allows you to create an account will be disabled until it is corrected!
     {
 		if (user.isOn == true) {
 			if (mailValidator.IsMatch (mail.text) && fname.text.Length > 1 && lname.text.Length > 1 && uname.text.Length > 5 && pword.text.Length > 7 && cpword.text.Length > 7 && cpword.text == pword.text && age () > 17  && myage.text.Length > 1) {
